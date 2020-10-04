3e0e6491fd10a93bc4c76e419793c24875333d91
peirong <liupeirongdg@163.com>
2020-10-04 23:16:33 +0800

    rollup test

--- rollup_components/src/index.js
+++ rollup_components/src/index.js
@@ -2,19 +2,19 @@ import Test from './components/Test/index'
 import Test2 from './components/Test2/index'
 import Test3 from './components/Test3/index'
 
-export default function(Vue) {
-    Vue.use(Test2)
-    Vue.use(Test3)
-    Vue.use(Test)
-}
-// export function test(Vue) {
+// export default function(Vue) {
+//     Vue.use(Test2)
+//     Vue.use(Test3)
 //     Vue.use(Test)
 // }
-// export function test2(Vue) {
-//     Vue.use(Test2)
-// }
+export function test(Vue) {
+    Vue.use(Test)
+}
+export function test2(Vue) {
+    Vue.use(Test2)
+}
 
-// export function test3(Vue) {
-//     Vue.use(Test3)
-// }
+export function test3(Vue) {
+    Vue.use(Test3)
+}
 
